704de7e30f968e1c424a3ab197dfa520e6e50b22
Ziqi Zhang <ziqi_zhang@pku.edu.cn>
2018-04-29 23:10:12 +0800

    initialize

--- auto_ssh.py
+++ auto_ssh.py
@@ -2,10 +2,8 @@ import pexpect
 import argparse
 
 IPs = {
-"bj15": "10.10.15.115",
-"bj17": "10.10.17.21",
-"sh30": "10.5.30.231",
-"office": "10.1.12.75"
+IP_alias_1: IP_1,
+IP_alias_2: IP_2
 }
 
 parser = argparse.ArgumentParser()
@@ -14,20 +12,12 @@ args = parser.parse_args()
 print(args.server)
 server = args.server
 ip = IPs[server]
-if server!="office":
-    user = "zhangziqi"
-    passwd = "Goodsense@"
-    mid_server_ip = "10.10.10.27"
-    child = pexpect.spawn('ssh %s@%s'%(user, mid_server_ip))
-    child.expect('zhangziqi@10.10.10.27\'s password:')
-    child.sendline(passwd)
-    child.expect('zhangziqi Please enter your Login Ip:')
-    child.sendline(ip)
-    child.interact()
-else:
-    user = "\"SENSETIME\zhangziqi\""
-    passwd = "Goodsense@"
-    child = pexpect.spawn('ssh %s@%s'%(user, ip))
-    child.expect('Password:')
-    child.sendline(passwd)
-    child.interact()
+user = "your username"
+passwd = "your password"
+mid_server_ip = "middle server ip"
+child = pexpect.spawn('ssh %s@%s'%(user, mid_server_ip))
+child.expect('Respond of middle server')
+child.sendline(passwd)
+child.expect('Respond about the server you are going to login')
+child.sendline(ip)
+child.interact()
